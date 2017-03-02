79d863fb1ccab29d3b931c1895aa4f8db4014462
arjun gupta <saimzn4241@gmail.com>
2017-03-02 11:38:16 +0530

    nav bar

---
+++ Hello World/Menu Bars/Menu Bars/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Menu Bars
+//
+//  Created by arjun gupta on 02/03/17.
+//  Copyright © 2017 arjun gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func CameraPressed(_ sender: Any) {
+        print ("camera pressed")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
