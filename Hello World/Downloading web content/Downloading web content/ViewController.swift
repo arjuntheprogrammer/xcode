b8e80f4976ba6949fbc4eb4b4b39a4f74857632c
arjun gupta <saimzn4241@gmail.com>
2017-03-03 18:24:55 +0530

    web view

---
+++ Hello World/Downloading web content/Downloading web content/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Downloading web content
+//
+//  Created by arjun gupta on 03/03/17.
+//  Copyright © 2017 arjun gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webview: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    //  let url = URL(string: "https://www.stackoverflow.com")!
+    //  webview.loadRequest(URLRequest(url: url))
+    //  webview.loadHTMLString("<h1>Hello there!!</h1>", baseURL: nil)
+       
+        if let url = URL(string: "https://www.stackoverflow.com"){
+         
+            let request = NSMutableURLRequest(url: url)
+            
+            let task = URLSession.shared.dataTask(with: request as URLRequest){
+                data , response, error in
+            
+                if error != nil{
+                    print(error)
+                }
+                
+                else{
+                    if let unwrappedData = data{
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                        print(dataString)
+                        DispatchQueue.main.sync(execute: {
+                            
+                        })
+                        
+                    }
+                }
+            }
+            task.resume()
+        }
+        print("hi there!")
+    
+    }
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
