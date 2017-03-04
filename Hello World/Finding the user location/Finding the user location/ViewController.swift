0ddc13ce930910d06a752c271b54e52b8051a58e
arjun gupta <saimzn4241@gmail.com>
2017-03-04 20:55:18 +0530

    current location

---
+++ Hello World/Finding the user location/Finding the user location/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Finding the user location
+//
+//  Created by arjun gupta on 04/03/17.
+//  Copyright © 2017 arjun gupta. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //print(locations)
+       
+        let userLocation: CLLocation = locations[0]
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.map.setRegion(region, animated: true)
+        
+        
+        
+        
+    }
+
+}
+
