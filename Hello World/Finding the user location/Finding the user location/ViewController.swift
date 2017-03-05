269e67cf9de4608db0055650245288588dabeeea
arjun gupta <saimzn4241@gmail.com>
2017-03-05 15:14:20 +0530

    more on location

--- Hello World/Finding the user location/Finding the user location/ViewController.swift
+++ Hello World/Finding the user location/Finding the user location/ViewController.swift
@@ -37,18 +37,70 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         //print(locations)
        
         let userLocation: CLLocation = locations[0]
-        let latitude = userLocation.coordinate.latitude
-        let longitude = userLocation.coordinate.longitude
+//        let latitude = userLocation.coordinate.latitude
+//        let longitude = userLocation.coordinate.longitude
         
-        let latDelta: CLLocationDegrees = 0.05
-        let lonDelta: CLLocationDegrees = 0.05
+//        let latDelta: CLLocationDegrees = 0.05
+//        let lonDelta: CLLocationDegrees = 0.05
         
-        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+//        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
         
-        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        let region = MKCoordinateRegion(center: location, span: span)
+//        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+//        let region = MKCoordinateRegion(center: location, span: span)
+//        
+//        self.map.setRegion(region, animated: true)
         
-        self.map.setRegion(region, animated: true)
+        CLGeocoder().reverseGeocodeLocation(userLocation){
+         (placemarks, error) in
+            if error != nil{
+                print(error!)
+            }
+            else{
+                if let placemark = placemarks?[0]{
+                    
+                    //print(placemark)
+                    //print(placemark.country!)
+                    
+                    var subThroughfare=""
+                   
+                    if placemark.subThoroughfare != nil{
+                        subThroughfare = placemark.subThoroughfare!
+                        
+                    }
+                    var throughfare=""
+                    if placemark.thoroughfare != nil{
+                        throughfare = placemark.thoroughfare!
+                        
+                    }
+                    
+                    var subLocality=""
+                    if placemark.subLocality != nil{
+                        subLocality = placemark.subLocality!
+                        
+                    }
+                    
+                    var subAdministrativeArea=""
+                    if placemark.subAdministrativeArea != nil{
+                        subAdministrativeArea = placemark.subAdministrativeArea!
+                    }
+                    var postalCode=""
+                    if placemark.postalCode != nil{
+                        postalCode = placemark.postalCode!
+                    }
+                    
+                    var country=""
+                    if placemark.country != nil{
+                        country = placemark.country!
+                    }
+                    
+                    print(subThroughfare + throughfare + "\n" + subLocality + "\n" + subAdministrativeArea + "\n" + postalCode + "\n" + country )
+                    
+                    
+                    
+                }
+            }
+            
+        }
         
         
         
