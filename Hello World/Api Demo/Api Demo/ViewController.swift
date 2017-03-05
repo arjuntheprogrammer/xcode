8921c4535b3eeef07e0a6155119b62f83c9fb349
arjun gupta <saimzn4241@gmail.com>
2017-03-05 23:51:33 +0530

    whats the weather through api

--- Hello World/Api Demo/Api Demo/ViewController.swift
+++ Hello World/Api Demo/Api Demo/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let url = URL(string: "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=3b38b91aeb9f6cd43660224bb8a9cb02")!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBAction func submit(_ sender: Any) {
+        
+        
+        if let url = URL(string: "http://samples.openweathermap.org/data/2.5/weather?q=" + cityTextField.text!.replacingOccurrences(of: " ", with: "%20") + ",uk&appid=3b38b91aeb9f6cd43660224bb8a9cb02"){
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             if error != nil{
@@ -23,31 +25,47 @@ class ViewController: UIViewController {
                 if let urlContent = data{
                     do{
                         let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String:Any]
-                    print(jsonResult)
-                    print(jsonResult["name"]!)
+                        print(jsonResult)
+                        print(jsonResult["name"]!)
                         
                         
                         if let weather = jsonResult["weather"] as? [[String: Any]], let description = weather.first?["description"] as? String {
-                            print(description)
+                            //print(description)
+                            DispatchQueue.main.sync(execute: {
+                                self.resultLabel.text = description
+                                
+                            })
+                            
                         }
-                       
-//                        if let description = jsonResult["weather"]?[0]["description"]{
-//                            
-//                            print(description)
-//                            
-//                        }
+                        
+                        //                        if let description = jsonResult["weather"]?[0]["description"]{
+                        //
+                        //                            print(description)
+                        //
+                        //                        }
                         
                     }
                     catch{
                         print("json processing failed")
                     }
                 }
-            
+                
             }
             
         }
         task.resume()
+      }
+        else{
+            resultLabel.text = "cant find weather for that city, try another"
+            
+        }
 
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
         
     }
 
