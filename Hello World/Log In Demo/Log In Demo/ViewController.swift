58068b4938c78901790c75d2b75f4677cb465af6
arjun gupta <saimzn4241@gmail.com>
2017-03-05 22:34:48 +0530

    login logout update username

--- Hello World/Log In Demo/Log In Demo/ViewController.swift
+++ Hello World/Log In Demo/Log In Demo/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var logoutButton: UIButton!
+    var isLoggedIn = false
+    
+    
+    
     @IBAction func Logout(_ sender: Any) {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
@@ -35,15 +39,77 @@ class ViewController: UIViewController {
                 
                 label.alpha = 0
                 logoutButton.alpha = 0
-                textField.alpha = 1
-                loginButton.alpha=1
-                
+                //textField.alpha = 1
+                //loginButton.alpha=1
+                loginButton.setTitle("Login", for: [])
+                isLoggedIn = false
             }
         }
         catch{
             print("delete failed")
         }
     }
+    
+    @IBAction func login(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        if isLoggedIn{
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+            
+            do{
+                let results = try context.fetch(request)
+                
+                if results.count > 0{
+                    for result in results as! [NSManagedObject]{
+                            result.setValue(textField.text, forKey: "name")
+                        do{
+                            try context.save()
+                        }
+                        catch{
+                            print("update username failed")
+                        }
+                        
+                    }
+                    label.text = "Hi there " + textField.text! + "!"
+                    
+                }
+                
+            }
+            catch{
+                print("update username failed")
+                
+            }
+        }
+            
+        else{
+        
+            let newValue = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+            newValue.setValue(textField.text, forKey: "name")
+            do {
+                try context.save()
+                //textField.alpha = 0
+                //lo    ginButton.alpha = 0
+                
+                loginButton.setTitle("Update username", for: [])
+            
+                label.alpha = 1
+                label.text = "Hi there " + textField.text! + "!"
+                logoutButton.alpha = 1
+            
+                isLoggedIn = true
+            
+            
+            }
+            catch{
+                print("failed to save")
+            }
+            
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -58,11 +124,14 @@ class ViewController: UIViewController {
             for result in results as! [NSManagedObject]{
                 if let username = result.value(forKey: "name") as? String{
                     
-                    textField.alpha = 0
-                    loginButton.alpha = 0
+                    //textField.alpha = 0
+                    //loginButton.alpha = 0
+                    
+                    loginButton.setTitle("Update username", for: [])
+                    logoutButton.alpha = 1
+
                     label.alpha = 1
                     label.text = "Hi there " + username + "!"
-                    logoutButton.alpha = 1
                     
                     
                 }
@@ -74,27 +143,7 @@ class ViewController: UIViewController {
     }
 
     @IBOutlet weak var loginButton: UIButton!
-    @IBAction func login(_ sender: Any) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
-        let newValue = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
-        newValue.setValue(textField.text, forKey: "name")
-        do {
-            try context.save()
-            textField.alpha = 0
-            loginButton.alpha = 0
-            label.alpha = 1
-            label.text = "Hi there " + textField.text! + "!"
-            logoutButton.alpha = 1
-            
-
-            
-        }
-        catch{
-            print("failed to save")
-        }
-        
-    }
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
