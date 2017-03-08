98d2f34c46fd0af14cde73cec9ff0e77f0764539
arjun gupta <saimzn4241@gmail.com>
2017-03-08 23:52:45 +0530

    swipe images

--- Hello World/TinderApp/ParseStarterProject/SwipingViewController.swift
+++ Hello World/TinderApp/ParseStarterProject/SwipingViewController.swift
@@ -12,8 +12,53 @@ import Parse
 
 class SwipingViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    func wasDragged(gestureRecognizer: UIPanGestureRecognizer ){
+        //print("was dragged")
+        let translation = gestureRecognizer.translation(in: view)
+        let label = gestureRecognizer.view!
+        
+        label.center = CGPoint(x: self.view.bounds.width / 2 + translation.x, y: self.view.bounds.height / 2 + translation.y)
+        let xFromCenter = label.center.x - self.view.bounds.width / 2
+        
+        
+        var rotation = CGAffineTransform(rotationAngle: xFromCenter / 200)
+        
+        let scale = min(abs(100 / xFromCenter),1)
+        
+        var streatchAndRotation = rotation.scaledBy(x: scale, y: scale)
+        label.transform = streatchAndRotation
+        
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.ended{
+            if label.center.x < 100{
+                print("not chosen")
+            }
+            else if label.center.x > self.view.bounds.width - 100{
+                print("chosen")
+            }
+            
+            rotation = CGAffineTransform(rotationAngle: 0)
+            streatchAndRotation = rotation.scaledBy(x: 1, y: 1)
+            label.transform = streatchAndRotation
+            
+            label.center = CGPoint(x: self.view.bounds.width / 2 , y: self.view.bounds.height/2)
+        }
+        //print(translation)
+    }
+    
+    func 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(self.wasDragged(gestureRecognizer:)))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(gesture)
+
 
         // Do any additional setup after loading the view.
     }
